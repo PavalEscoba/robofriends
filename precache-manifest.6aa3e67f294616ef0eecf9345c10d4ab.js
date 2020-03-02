d474a3bfe6b043bfed377b3d9955b011466a3063
Paval Miatlitski <paval.escoba@gmail.com>
2020-03-03 01:28:14 +0300

    Updates

---
+++ precache-manifest.6aa3e67f294616ef0eecf9345c10d4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e4c7975afe3cce37c787025432d51b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "09dbef6361b81fca2a90",
+    "url": "/robofriends/static/css/main.b11d4b03.chunk.css"
+  },
+  {
+    "revision": "8b1d9f1f6a2bcbae90c4",
+    "url": "/robofriends/static/js/2.8f5c723e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.8f5c723e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09dbef6361b81fca2a90",
+    "url": "/robofriends/static/js/main.3a27cace.chunk.js"
+  },
+  {
+    "revision": "5677d8ff5cb4c373cfaa",
+    "url": "/robofriends/static/js/runtime-main.c712519d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
